4e4d2efeaf93fbd01cd4d2eee021fc6f053cec9c
Pranjal Yadav <99741190+pranjalyadav-py@users.noreply.github.com>
2024-01-23 22:27:41 +0530

    Time: 13 ms (78.39%) | Memory: 15.6 MB (66.91%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
     int n;
-    bool hasCommon(string &s1, string& s2) {
-        int arr[26] = {0};
-        
-        for(char &ch : s1) {
-            if(arr[ch-'a'] > 0)
-                return true;
-            arr[ch-'a']++;
+    bool dup(string &s1,string &s2){
+        int drr[26] = {0};
+        for(char &ch:s1)
+        {
+            if(drr[ch-'a']>0)
+            return true;
+
+            drr[ch-'a']++;
         }
-        
-        for(char &ch : s2) {
-            if(arr[ch-'a'] > 0)
-                return true;
+
+        for(char &ch:s2)
+        {
+            if(drr[ch-'a'])
+            return true;
         }
-        
+
         return false;
     }
 
-    int func(int idx, vector<string>& arr, string temp) {
-        if(idx >= n)
-            return temp.length();
+    int func(string temp,int idx,vector<string>& arr){
+       
+        if(idx>=n)
+        return temp.length();
 
         int exclude = 0;
         int include = 0;
 
-        if(hasCommon(arr[idx],temp))
-        exclude = func(idx+1,arr,temp);
+        if(dup(arr[idx],temp))
+        exclude = func(temp,idx+1,arr);
         else
         {
-            exclude = func(idx+1,arr,temp);
+            exclude = func(temp,idx+1,arr);
             temp+=arr[idx];
-            include = func(idx+1,arr,temp);
+            include = func(temp,idx+1,arr);
         }
+
         return max(exclude,include);
     }
 
     int maxLength(vector<string>& arr) {
-        string temp = "";
         n = arr.size();
-        return func(0,arr,temp);
+        return func("",0,arr);
     }
 };
