5fdc46bd747994826418294e0799b74a1c80d0e8
Pranjal Yadav <99741190+pranjalyadav-py@users.noreply.github.com>
2024-02-22 00:39:09 +0530

    Time: 3 ms (78.07%) | Memory: 13.6 MB (7.37%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-         ios_base::sync_with_stdio(false); 
-         cin.tie(NULL);                    
-         cout.tie(NULL);
+ios_base::sync_with_stdio(false);  cin.tie(NULL);cout.tie(NULL);
         bool z = 0;
         int cnt=1;
         int j =0;
