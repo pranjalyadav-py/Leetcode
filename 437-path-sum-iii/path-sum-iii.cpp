935edb56bb8fa4ff943346c146bc573f396a2709
Pranjal Yadav <99741190+pranjalyadav-py@users.noreply.github.com>
2024-02-23 22:15:31 +0530

    Time: 21 ms (46.60%) | Memory: 17.4 MB (64.82%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int count  = 0;
+    void runsum(TreeNode* root, int targetSum,long long cursum,int& count)
+    {
+        if(root == nullptr)
+        return;
+
+        cursum+=root->val;
+        if(cursum==targetSum)
+        count++;
+
+        runsum(root->left,targetSum,cursum,count);
+        runsum(root->right,targetSum,cursum,count);
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+
+       if(root == nullptr)
+       return count;
+
+       runsum(root,targetSum,0,count);
+       pathSum(root->left,targetSum);
+       pathSum(root->right,targetSum);
+
+       return count; 
+    }
+};
