0a383193b02866cb1f645b22bfed5afc6a514652
Pranjal Yadav <99741190+pranjalyadav-py@users.noreply.github.com>
2024-03-04 18:43:13 +0530

    Time: 2 ms (88.24%) | Memory: 13.2 MB (23.53%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.cpp
+++ 985-bag-of-tokens/bag-of-tokens.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
-
+        ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
         if(tokens.size()==0)
         return 0;
-        
+
         sort(tokens.begin(),tokens.end());
         int n = tokens.size();
         int i =0,j =n-1;
