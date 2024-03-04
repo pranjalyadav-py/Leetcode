baac57250cb72bd8347ce1f7bd8432d1912b3829
Pranjal Yadav <99741190+pranjalyadav-py@users.noreply.github.com>
2024-03-04 18:38:36 +0530

    Time: 3 ms (86.63%) | Memory: 13.1 MB (23.53%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+
+        if(tokens.size()==0)
+        return 0;
+        
+        sort(tokens.begin(),tokens.end());
+        int n = tokens.size();
+        int i =0,j =n-1;
+        int score =0;
+
+        if(power>=tokens[0])
+        {
+            power = power - tokens[0];
+            score++;
+            i++;
+        }
+
+        while((score>0 || power>=tokens[i]) && i<=j)
+        {
+            if(power<tokens[i])
+            {
+                score--;
+                power+=tokens[j];
+                j--;
+            }
+            
+            if(power>=tokens[i])
+            {
+                score++;
+                power-=tokens[i];
+                i++;
+            }
+        }
+        return score;
+    }
+};
