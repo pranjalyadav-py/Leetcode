52af3db8004b4f40b8ca42ce1dba8c055c9cb7c1
Pranjal Yadav <99741190+pranjalyadav-py@users.noreply.github.com>
2024-03-07 22:18:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (7.87%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
         int n = 0;
 
         ListNode* temp = head;
