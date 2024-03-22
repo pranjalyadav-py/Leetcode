a988a212244b01ec82441bec13c1f1461014d2ca
Pranjal Yadav <99741190+pranjalyadav-py@users.noreply.github.com>
2024-03-22 15:18:04 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (12.23%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+    
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    
+    int n = nums.size();
+    int pref = 1, suff = 1;
+	int ans = INT_MIN;
+	for(int i =0;i<n;i++)
+	{
+		if(pref ==0)
+		pref = 1;
+		if(suff ==0)
+		suff = 1;
+
+		pref = pref * nums[i];
+		suff = suff * nums[n- i - 1];
+
+		ans = max(ans,max(pref,suff));
+	}
+
+	return ans;
+    }
+};
